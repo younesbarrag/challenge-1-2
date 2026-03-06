c96ba97460109b7ce1d79ad9812e1e4e6d936070
younesbarrag <younesbarrag003gmail.com>
2026-03-06 07:16:17 +0000

    challenge php

---
+++ chal3.php
@@ -0,0 +1,18 @@
+<?php
+$debts = ["younes" => 50, "meriem" => 120, "Yassine" => 80, "Leila" => 250, "Omar" => 30];
+$total = 0;
+
+foreach ($debts as $name => $amount) {
+    $total += $amount;
+    
+    echo $name . ": " . $amount . " DH";
+    
+    if ($amount > 100) {
+        echo " --- (KHLASS!)"; 
+    }
+    
+    echo "\n"; 
+}
+
+echo "Total: " . $total . " DH";
+?>
